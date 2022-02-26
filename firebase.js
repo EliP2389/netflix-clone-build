6d83e97c1cbf4b5ba8e4e6fbb6944883140bf246
Eli Pagan <mbk8689@gmail.com>
2022-02-26 11:53:58 -0500

    initial project set up

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCztdACVqSGySZ_Pen3-5o-T9TvuZfHOEg",
+    authDomain: "netflix-clone-build-2389.firebaseapp.com",
+    projectId: "netflix-clone-build-2389",
+    storageBucket: "netflix-clone-build-2389.appspot.com",
+    messagingSenderId: "396349024573",
+    appId: "1:396349024573:web:984c7692c17cebce047d54",
+    measurementId: "G-PC9XNTPDBV"
+  };
