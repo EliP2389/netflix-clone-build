b02b78985d270d7dbfb9f817fb92f018f47c0a88
Eli Pagan <mbk8689@gmail.com>
2022-02-26 14:13:52 -0500

    added styling to nav content for Nav.js and useState and useEffect

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.css';
 
 function Nav() {
+const [show, handleShow] = useState(false);
+
+const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+        handleShow(true);
+    } else {
+        handleShow(false);
+    }
+}
+
+useEffect (() => {
+    window.addEventListener('scroll', transitionNavBar);
+    // clean up function
+    return () => window.removeEventListener('scroll', transitionNavBar);
+}, []);
+
     return (
-        <div className='nav'>
+        <div className='nav nav_black'>
             <div className='nav_content'>
                 <img className='nav_logo'
                     src='https://assets.brand.microsites.netflix.io/assets/7dc497e2-4975-11ec-a9ce-066b49664af6_cm_1440w.jpg?v=5'
