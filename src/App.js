2743021ec674c5669a79ea8d08ca7ad04a8b9816
Eli Pagan <mbk8689@gmail.com>
2022-03-08 20:22:43 -0500

    added react router throught npm and initial starting point on App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './components/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
-    <HomeScreen />
+      <HomeScreen />
+      <Router>
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/users'>
+            <Users />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
