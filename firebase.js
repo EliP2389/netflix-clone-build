2106bb18d348789d81ac3b803616a23325f16998
Eli Pagan <mbk8689@gmail.com>
2022-03-10 13:11:07 -0500

    update firbase.js to handle authentication

--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCztdACVqSGySZ_Pen3-5o-T9TvuZfHOEg",
@@ -8,3 +10,10 @@ const firebaseConfig = {
     appId: "1:396349024573:web:984c7692c17cebce047d54",
     measurementId: "G-PC9XNTPDBV"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth }
+  export default db;
