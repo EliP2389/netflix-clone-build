aecad8c1e91949937c66e1d4afbc1ad99b8c0812
Eli Pagan <mbk8689@gmail.com>
2022-03-10 11:45:05 -0500

    updated readme and signupscreen form and styling

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './LoginScreen.css';
-import SignInScreen from './SignInScreen';
+import SignUpScreen from './SignUpScreen';
 
 function LoginScreen() {
     const [signIn, setSignIn] = useState(false);
@@ -10,7 +10,7 @@ function LoginScreen() {
             <div className='loginScreen_background'>
                 <img
                     className='loginScreen_logo'
-                    src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
+                    src='https://thereelbits.com/wp-content/uploads/2017/05/Netflix-Logo.jpg'
                     alt='' />
                 <button onClick={() => setSignIn(true)}
                     className='loginScreen_button'>
@@ -22,7 +22,7 @@ function LoginScreen() {
 
             <div className='loginScreen_body'>
                 {signIn ? (
-                    <SignInScreen />
+                    <SignUpScreen />
                 ) : (
                     <>
                         <h1>Unlimited films, TV programs and more</h1>
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SignUpScreen.css';
+
+export default function SignUpScreen() {
+  return (
+    <div className='signUpScreen'>
+      <form>
+        <h1>Sign In</h1>
+        <input placeholder='Email' type='email' />
+        <input placeholder='Password' type='password' />
+        <button type='submit'>Sign In</button>
+      </form>
+    </div>
+  )
+}
